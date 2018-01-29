2b1f9293cc0096ac3ed7fe2eb428df39de8697c7
Adam Karpierz <akarpierz@gmail.com>
2018-01-29 05:19:31 +0100

    ...
    ...

---
+++ src/jt/jep/_java/class2py.py
@@ -0,0 +1,14 @@
+from jt.jvm.java.class2py import *
+
+header = \
+"""\
+# Copyright (c) 2014-2018 Adam Karpierz
+# Licensed under the zlib/libpng License
+# http://opensource.org/licenses/zlib
+
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    class2py(sys.argv[1], header=header)
